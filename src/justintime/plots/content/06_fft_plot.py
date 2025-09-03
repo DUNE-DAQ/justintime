492b481240796169312ca1ec84d0defedd262c45
Alessandro Thea <alessandro.thea@cern.ch>
2025-09-03 10:05:01 +0200

    Poloshing up fft plot

--- src/justintime/plots/content/06_fft_plot.py
+++ src/justintime/plots/content/06_fft_plot.py
@@ -22,7 +22,6 @@ def return_obj(dash_app, engine, storage,theme):
     plot.add_ctrl("partition_select_ctrl")
     plot.add_ctrl("run_select_ctrl")
     plot.add_ctrl("06_trigger_record_select_ctrl")
-    plot.add_ctrl("23_apa_select_ctrl")
     plot.add_ctrl("90_plot_button_ctrl")
 
     init_callbacks(dash_app, storage, plot_id,theme)
@@ -37,10 +36,9 @@ def init_callbacks(dash_app, storage, plot_id,theme):
         State("run_select_ctrl","value"),
         State('trigger_record_select_ctrl', "value"),
         State('file_select_ctrl', "value"),
-        State('apa_select_ctrl', "value"),
         State(plot_id, "children"),
     )
-    def plot_fft_graph(n_clicks, refresh, partition, run, trigger_record, raw_data_file, elem_name, original_state):
+    def plot_fft_graph(n_clicks, refresh, partition, run, trigger_record, raw_data_file, original_state):
 
         load_figure_template(theme)
         if trigger_record and raw_data_file:
@@ -51,51 +49,68 @@ def init_callbacks(dash_app, storage, plot_id,theme):
 
                 det_keys = [ key for key in data.df_dict if key.startswith('detw') and 'TPC' in key ]
                 print(det_keys)
-                print(elem_name)
 
 
-                # FIXME: HORRROR!!!
-                element_id = int(elem_name[3]) #assuming APAX or CRPX
+                # # FIXME: HORRROR!!!
+                # element_id = int(elem_name[3]) #assuming APAX or CRPX
 
                 # Find all datasets for the selected element
-                df_sel = {k:data.df_dict[k] for k in det_keys if element_id in data.df_dict[k].element.unique()}
+                # df_sel = {k:data.df_dict[k] for k in det_keys if element_id in data.df_dict[k].element.unique()}
 
-                if len(df_sel) == 0:
-                    raise RuntimeError(f"No dataset matching {elem_name} found")
-                elif len(df_sel) > 1:
-                    raise RuntimeError(f"Found multiple datasets matching {elem_name}")
+                # if len(df_sel) == 0:
+                #     raise RuntimeError(f"No dataset matching {elem_name} found")
+                # elif len(df_sel) > 1:
+                #     raise RuntimeError(f"Found multiple datasets matching {elem_name}")
                 
-                df = next(iter(df_sel.values()))
-                freqs = fft.rfftfreq(len(df.iloc[0].adcs), 512e-9)
 
-                print(df)
+                # import IPython
+                # IPython.embed(colors='neutral')
+
+                df_elems = {}
+                for key in det_keys:
+                    df_els = {el:df_el for el, df_el in data.df_dict[key].groupby(by='element')}
+                    overlap = (set(df_elems.keys()) & set(df_els.keys()))
+                    if overlap:
+                        raise RuntimeError(f'Found duplicated elements in different detector datasets {overlap}')
+                    df_elems.update(df_els)
 
 
-                df_fft = pd.DataFrame(df.groupby('plane').fft_mag.mean()).T.explode([0,1,2]).reset_index()
-                df_fft['freq'] = freqs
 
-                title=f"FFT : Run {run}: {trigger_record}" 
-
-                fig = px.line(
-                    df_fft,
-                    x='freq', 
-                    y=[0,1,2], 
-                    log_y=True, 
-                    title=title, 
-                    labels={
-                     "value": "Magnitude",
-                     "freq": "Frequency (Hz)",
-                    },
-                 )
-                add_dunedaq_annotation(fig)
-
-                fig.update_layout(font_family="Lato", title_font_family="Lato")
-                return(html.Div([
+                children = [
                     selection_line(partition,run,raw_data_file, trigger_record),
-                    html.B("FFT by plane"),
-                    dcc.Graph(figure=fig),
+                ]
+                for elem_id, df in df_elems.items():
+                    freqs = fft.rfftfreq(len(df.iloc[0].adcs), 512e-9)
 
-                ]))
+                    df_fft = pd.DataFrame(df.groupby('plane').fft_mag.mean()).T.explode([0,1,2]).reset_index()
+                    # df_fft.columns = ['Frequency', 'Plane U', 'Plane V', 'Plane Z']
+                    df_fft['freq'] = freqs
+
+
+                    # FIXME: hardcoded CRP
+                    title=f"FFT CRP {elem_id}: Run {run}: {trigger_record}" 
+
+                    fig = px.line(
+                        df_fft.iloc[1:],
+                        # x='Frequency', 
+                        # y=['Plane U', 'Plane V', 'Plane Z'], 
+                        x='freq', 
+                        y=[0,1,2], 
+                        log_y=True, 
+                        title=title, 
+                        labels={
+                        "value": "Magnitude",
+                        "freq": "Frequency (Hz)",
+                        },
+                    )
+                    add_dunedaq_annotation(fig)
+                    fig.update_layout(font_family="Lato", title_font_family="Lato")
+
+                    children += [
+                            html.B("FFT by plane"),
+                            dcc.Graph(figure=fig),     
+                         ]
+                return(html.Div(children))
             else:
                 return(html.Div(html.H6(nothing_to_plot())))
             return(original_state)
